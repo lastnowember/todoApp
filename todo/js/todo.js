67c1f009625da3374725b2a1358c161acda69c64
lastnowember <lastnowember@gmail.com>
2016-09-01 02:15:41 +0300

    checboxes in procces

--- todo/js/todo.js
+++ todo/js/todo.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 	$('.addtask').click(function() {
 		var newTask = $('input[name=newtask]').val();
-		var toAdd = '<div class="taskone"><input type="checkbox" class="checktask" name=""><p class="task">' + newTask + '</p></div>';
-		$('.wraptask').append(toAdd);
+		var toAdd = '<div class="taskone"><input type="checkbox" class="checktask" name="checky"><p class="task">' + newTask + '</p></div>';
+		$('.wraptask').prepend(toAdd);
+	});
+
+	$('input[type="checkbox"]').click(function() {
+		var check = $('input[type="checkbox"]').prop('checked');
+		if (check == true) {
+			$('.task').css("text-decoration","line-through");
+		}
 	});
 });
