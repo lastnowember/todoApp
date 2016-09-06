4d3f20a72b43e3e9354b08ab182c1adbb32abfbe
lastnowember <lastnowember@gmail.com>
2016-09-06 19:29:26 +0300

    delete text task in input after add task

--- todo/js/todo.js
+++ todo/js/todo.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 	$('.addtask').click(function() {
-		var newTask = $('input[name=newtask]').val();
+		var inputTask = $('input[name=newtask]')
+		var newTask = $(inputTask).val();
+		var place = $('.inputask').attr('placeholder');
 		var toAdd = '<div class="taskone"><input type="checkbox" class="checktask" name="checky"><p class="task">' + newTask + '</p></div>';
 		$('.wraptask').prepend(toAdd);
+		$(inputTask).val('');
 	});
 
 	$('input[type="checkbox"]').click(function() {
