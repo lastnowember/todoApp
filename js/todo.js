fc8f7f08ef705d39b833a8098f18f17e5dd7af10
lastnowember <lastnowember@gmail.com>
2016-09-06 20:57:36 +0300

    t

---
+++ js/todo.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$('.addtask').click(function() {
+		var inputTask = $('input[name=newtask]')
+		var newTask = $(inputTask).val();
+		var place = $('.inputask').attr('placeholder');
+		var toAdd = '<div class="taskone"><input type="checkbox" class="checktask" name="checky"><p class="task">' + newTask + '</p></div>';
+		$('.wraptask').prepend(toAdd);
+		$(inputTask).val('');
+	});
+
+	$('input[type="checkbox"]').click(function() {
+		var check = $('input[type="checkbox"]').prop('checked');
+		console.log(check);
+		if (check == true) {
+			$('.task').css("text-decoration","line-through");
+		}
+	});
+});
