961f33f721cb7b52f1038ec14b625f7c49a15cca
lastnowember <lastnowember@gmail.com>
2016-09-15 12:15:47 +0300

    new display tasks but app not work :(

--- js/todo.js
+++ js/todo.js
@@ -1,18 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function() { /*
+		 */
+	var n = 1;
 	$('.addtask').click(function() {
 		var inputTask = $('input[name=newtask]')
 		var newTask = $(inputTask).val();
 		var place = $('.inputask').attr('placeholder');
-		var toAdd = '<div class="taskone"><input type="checkbox" class="checktask" name="checky"><p class="task">' + newTask + '</p></div>';
+		var toAdd = '<div id="taskdata" class="taskone" data-tasks="' + n + '"><a href="#" id="checklink" class="checki"><img src="./img/check.png" id="icheck" aria-hidden="true"></a><p class="task">' + newTask + '</p></div>';
 		$('.wraptask').prepend(toAdd);
 		$(inputTask).val('');
+		n = n + 1;
 	});
 
-	$('input[type="checkbox"]').click(function() {
-		var check = $('input[type="checkbox"]').prop('checked');
-		console.log(check);
-		if (check == true) {
-			$('.task').css("text-decoration","line-through");
+	var idTask = $('#taskdata').data('tasks');
+
+	$('.wraptask', this).on('click', 'img', function() {
+		var linkCheck = $('#checklink', this).attr('class');
+		var idTask = $('#taskdata').data('tasks');
+		if (linkCheck == 'checky') {
+			$('#icheck', this).attr('src','./img/check.png');
+			$('.task', this).css('text-decoration','none');
+			$('.task', this).css('color','#000000');
+			$('#checklink', this).attr('class', 'checki');
+		} else {
+			$('#icheck', this).attr('src','./img/check_2.png');
+			$('.task', this).css('text-decoration','line-through');
+			$('.task', this).css('color','#cccccc');
+			$('#checklink', this).attr('class', 'checky');
 		}
+		
 	});
+
+
 });
